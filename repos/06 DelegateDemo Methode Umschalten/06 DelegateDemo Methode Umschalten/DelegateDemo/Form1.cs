ff30508dc6bd35a2d4b559f748d81c78780ddab4
Vinc13ent <58013142+Vinc13ent@users.noreply.github.com>
2021-04-12 12:43:34 +0200

    Add Delegate_demo

---
+++ repos/06 DelegateDemo Methode Umschalten/06 DelegateDemo Methode Umschalten/DelegateDemo/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegateDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        delegate void MyDelegate(int i);
+
+        private void EineMethode(int i)
+        {
+            textBox1.Text += "EineMethode wurde aufgerufen: " + i;
+            textBox1.Text += Environment.NewLine;
+        }
+        private void EineAndereMethode(int i)
+        {
+            textBox1.Text += "EineAndereMethode wurde aufgerufen: " + i;
+            textBox1.Text += Environment.NewLine;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyDelegate verweisAufEineMethode = EineMethode;
+            verweisAufEineMethode(5);
+            verweisAufEineMethode = EineAndereMethode;
+            verweisAufEineMethode(5);
+        }
+    }
+}

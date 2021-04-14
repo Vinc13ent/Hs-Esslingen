f0543426c220fdf3c155c81666f6374b8a30210e
Vinc13ent <58013142+Vinc13ent@users.noreply.github.com>
2021-04-14 18:08:22 +0200

    aufgaben eingefügt von teilaufgabe 2 events

---
+++ repos/Labor_01_Teil1_Serielle_kommunikation/Labor_01_Teil1_Serielle_kommunikation/DemoDevice.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Labor_01_Teil1_Serielle_kommunikation
+{
+    internal class DemoDevice
+    {
+        private enum CommandBytes : byte
+        {
+            SetCounterZero = 0x7A,
+            CounterIncrementDown = 0x7B,
+            CounterIncrementUp = 0x7C,
+            SendCounterNumber = 0x7D,
+            SendSerialNumber = 0x7E,
+            SendDeviceName = 0x7F,
+        }
+
+        public enum ConnectionStates
+        {
+            Connected = 1,
+            Disconnected = 0,
+            Connecting = -1,
+        }
+
+        private string _SerialNumber;
+        public string SerialNumber
+        {
+            get
+            {
+                return _SerialNumber;
+            }
+            set
+            {
+                _SerialNumber = value;
+                OnPropertyChanged(_SerialNumber);
+            }
+        }
+
+        private string _DeviceName;
+        public string DeviceName
+        {
+            get
+            {
+                return _DeviceName;
+            }
+            set
+            {
+                _DeviceName = value;
+                OnPropertyChanged(_DeviceName);
+            }
+        }
+
+        private int _CurrentNumber;
+        public int CurrentNumber
+        {
+            get
+            {
+                return _CurrentNumber;
+            }
+            set
+            {
+                _CurrentNumber = value;
+                OnPropertyChanged(_CurrentNumber.ToString());
+            }
+        }
+
+        private ConnectionStates _ConnectionState;
+        public ConnectionStates ConnectionState
+        {
+            get
+            {
+                return _ConnectionState;
+            }
+            set
+            {
+                OnPropertyChanged(_ConnectionState.ToString());
+                _ConnectionState = value;
+            }
+        }
+
+        SerialPort serialPort = new SerialPort();
+
+        public delegate void PropertyChangedHandler(DemoDevice source, string propertyName);
+
+        public event PropertyChangedHandler PropertyChanged; 
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, propertyName);
+            }
+        }
+
+        public void Connect(int portNumber)
+        {
+            try
+            {
+                string comname = "COM" + portNumber;
+                serialPort.PortName = comname;
+                serialPort.BaudRate = 9600;
+                ConnectionState = ConnectionStates.Connecting;
+                serialPort.Open();
+                if (serialPort.IsOpen)
+                { 
+                    ConnectionState = ConnectionStates.Connected;
+                    ReadDeviceInfo();
+                }
+            }
+            catch
+            { 
+                Console.WriteLine("Verbindung fehlgeschlagen");
+                ConnectionState = ConnectionStates.Disconnected;
+            }
+        }
+
+
+        private void ReadDeviceInfo()
+        {
+            byte[] commandName = new byte[] {(byte) CommandBytes.SendDeviceName};
+            byte[] commandSerialNumber = new byte[] {(byte) CommandBytes.SendSerialNumber};
+            byte[] commandCounterNumber = new byte[] {(byte) CommandBytes.SendCounterNumber};
+            serialPort.Write(commandName, 0, 1);
+            DeviceName = serialPort.ReadLine();
+            serialPort.Write(commandSerialNumber, 0, 1);
+            SerialNumber = serialPort.ReadLine();
+            serialPort.Write(commandCounterNumber, 0, 1);
+            CurrentNumber = int.Parse(serialPort.ReadLine());
+        }
+
+        public void Disconnect()
+        {
+            serialPort.Close();
+            ConnectionState = ConnectionStates.Disconnected;
+        }
+
+        public void Increment()
+        {
+            if (serialPort.IsOpen) 
+            {
+                byte[] command = new byte[] { (byte)CommandBytes.CounterIncrementUp };
+                serialPort.Write(command, 0, 1); 
+                command = new byte[] { (byte)CommandBytes.SendCounterNumber};
+                serialPort.Write(command, 0, 1);
+                CurrentNumber = int.Parse(serialPort.ReadLine());
+            }
+            else
+            {
+         
+            }
+        }
+
+        public void Decrement()
+        {
+            if (serialPort.IsOpen)
+            {
+                byte[] command = new byte[] { (byte)CommandBytes.CounterIncrementDown };
+                serialPort.Write(command, 0, 1);
+                command = new byte[] { (byte)CommandBytes.SendCounterNumber};
+                serialPort.Write(command, 0, 1);
+                CurrentNumber = int.Parse(serialPort.ReadLine());
+            }
+            else
+            {
+
+            }
+        }
+        
+        public void Reset()
+        {
+            if (serialPort.IsOpen)
+            {
+                byte[] command = new byte[] { (byte)CommandBytes.SetCounterZero };
+                serialPort.Write(command, 0, 1);
+            }
+            else
+            {
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DemoDeviceTester());
+        }
+    }
+}
--- repos/Labor_01_Teil1_Serielle_kommunikation/Labor_01_Teil1_Serielle_kommunikation/DemoDeviceTester.cs
+++ repos/Labor_01_Teil1_Serielle_kommunikation/Labor_01_Teil1_Serielle_kommunikation/DemoDeviceTester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,32 +8,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Labor_01_Teil1_Serielle_kommunikation
 {
-    public partial class DemoDeviceTester : Form
+    public partial class DemoDeviceTester : Form 
     {
 
+        private DemoDevice _device;
+        
         public DemoDeviceTester()
         {
+            _device = new DemoDevice();
+            _device.PropertyChanged += OnDevicePropertyChanged;
             InitializeComponent();
         }
-
-        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-            ToolStripStatusLabel StatusLableConection = new System.Windows.Forms.ToolStripStatusLabel();
-            ToolStripStatusLabel StatusLableDevice = new System.Windows.Forms.ToolStripStatusLabel();
-            ToolStripStatusLabel StatusLableNR = new System.Windows.Forms.ToolStripStatusLabel();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void DemoDeviceTester_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            _device.Increment();
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            _device.Decrement();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            _device.Reset();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            int port = Convert.ToInt32(comUpDown.Value);
+            _device.Connect(port);
+            ConectionStateLabel.Text = _device.ConnectionState.ToString();
+            DeviceLabel.Text = "Name: " + _device.DeviceName.ToString();
+            SerialNumberLabel.Text = "S/N: " + _device.SerialNumber.ToString();
+        }
+
+        private void comUpDown_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        public void OnDevicePropertyChanged(object sender, string propertyName)
+        {
+            Console.WriteLine(propertyName);
+            CounterTextBox.Text = propertyName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+        }
+        
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+        }
+        
+        private void toolStripStatusLabel3_Click(object sender, EventArgs e)
+        {
+        }
+
+        
     }
 }
